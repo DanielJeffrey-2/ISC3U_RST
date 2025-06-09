a1ad5ec74383205f0b1b13379d00dcecf9c88e06
DanielJeffrey-2 <113033888+DanielJeffrey-2@users.noreply.github.com>
2025-06-09 13:55:04 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Created by: D. Jeffrey
+Created on: June 2025
+This constants file is for the Tron game
+"""
+
+# PyBadge screen size is 160 x 128 and sprites are 16 x 16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
