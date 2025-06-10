90a87be6d4f813234bbb5cf322700fceaf6cff67
DanielJeffrey-2 <113033888+DanielJeffrey-2@users.noreply.github.com>
2025-06-10 14:46:48 -0400

    Started tutorial 8

--- game.py
+++ game.py
@@ -8,14 +8,86 @@ This module is a TRON clone
 
 import stage
 import ugame
+import time
+import random
 
 import constants
 
 
+def splash_scene() -> None:
+    """ This function is the splash scene """
+    
+    coin_sound = open("coin.wav, 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+    
+    
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+   
+    game = stage.Stage(ugame.display, constants.FPS)
+
+    game.layers = text + [background]
+
+    game.render_block()
+
+    while True:
+
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_START != 0:
+            game_scene()
+
+        game.tick()
+    
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = [background]
+    game.render_block()
+    
+    while True:
+        # get user input
+        time.sleep(2.0)
+        menu_scene()
+
+        # redraw sprites
+        game.tick()
+        
 def menu_scene() -> None:
     """ This function is the main game game_scene """
     
-    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
 
     text = []
     text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
@@ -30,7 +102,7 @@ def menu_scene() -> None:
     
     
     # putting background on screen
-    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
     
     game = stage.Stage(ugame.display, constants.FPS)
     game.layers = text + [background]
@@ -60,7 +132,7 @@ def game_scene() -> None:
     select_button = constants.button_state["button_up"]
     
     #sound
-    pew_sound = open("pew.wav", "rb")
+    pew_sound = open("pew.wav", 'rb')
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
